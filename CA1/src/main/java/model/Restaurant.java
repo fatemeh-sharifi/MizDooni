4ff85cb97a731ba28ee59188d7e149cd81623105
baharvand79 <shakibabaharvand2000@gmail.com>
2024-02-24 21:42:44 +0330

    feat(Restaurant): attributes are added to the Restaurant class

--- CA1/src/main/java/model/Restaurant.java
+++ CA1/src/main/java/model/Restaurant.java
@@ -1,4 +1,79 @@
 package model;
 
 public class Restaurant {
+    private String name;
+    private String managerUsername;
+    private String type;
+    private String startTime;
+    private String endTime;
+    private String description;
+    private String address;
+
+    public Restaurant(String name, String managerUsername, String type, String startTime, String endTime, String description, String address) {
+        this.name = name;
+        this.managerUsername = managerUsername;
+        this.type = type;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.description = description;
+        this.address = address;
+    }
+
+    // Getters and setters for attributes
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManagerUsername() {
+        return managerUsername;
+    }
+
+    public void setManagerUsername(String managerUsername) {
+        this.managerUsername = managerUsername;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
