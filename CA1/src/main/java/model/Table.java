dd9701c762244d528d982923f77be5002e447c59
baharvand79 <shakibabaharvand2000@gmail.com>
2024-02-24 21:50:16 +0330

    feat(): Table.java is created and attributes are added

---
+++ CA1/src/main/java/model/Table.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Table {
+    private int tableNumber;
+    private String restaurantName;
+    private String managerUsername;
+    private int seatsNumber;
+
+    public Table(int tableNumber, String restaurantName, String managerUsername, int seatsNumber) {
+        this.tableNumber = tableNumber;
+        this.restaurantName = restaurantName;
+        this.managerUsername = managerUsername;
+        this.seatsNumber = seatsNumber;
+    }
+
+    // Getters and setters for attributes
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getManagerUsername() {
+        return managerUsername;
+    }
+
+    public void setManagerUsername(String managerUsername) {
+        this.managerUsername = managerUsername;
+    }
+
+    public int getSeatsNumber() {
+        return seatsNumber;
+    }
+
+    public void setSeatsNumber(int seatsNumber) {
+        this.seatsNumber = seatsNumber;
+    }
+}
