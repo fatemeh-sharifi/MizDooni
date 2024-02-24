2dbbf5c017490bbdf9fb8afc15e06a0bda341d75
baharvand79 <shakibabaharvand2000@gmail.com>
2024-02-24 21:06:46 +0330

    feat(): User class is created and attributes are added.

---
+++ CA1/src/main/java/model/User.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.util.List;
+
+public class User {
+    private String username;
+    private String email;
+    private String password;
+    private List<Reservation> reservations;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and setters for attributes
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+
+}
