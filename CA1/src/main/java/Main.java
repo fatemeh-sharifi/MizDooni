9459b8e22c0a87de0dec376a5c22d87e763ccdc7
baharvand79 <shakibabaharvand2000@gmail.com>
2024-02-25 12:44:32 +0330

    feat(Main): different inputs are added to see the results of adding restaurants

--- CA1/src/main/java/Main.java
+++ CA1/src/main/java/Main.java
@@ -4,9 +4,39 @@ public class Main {
     public static void main(String[] args) {
         // Sample JSON data for adding a user
         String jsonData = "{\"role\": \"client\", \"username\": \"user1\", \"password\": \"1234\", \"email\": \"user1@gmail.com\", \"address\": {\"country\": \"Iran\", \"city\": \"Tehran\"}}";
-
-        // Call the addUser method and print the result
         String result = UserManagement.addUser(jsonData);
         System.out.println(result);
+
+        // Sample JSON data for adding a user : error client/manager
+        jsonData = "{\"role\": \"user\", \"username\": \"user1\", \"password\": \"1234\", \"email\": \"user1@gmail.com\", \"address\": {\"country\": \"Iran\", \"city\": \"Tehran\"}}";
+        result = UserManagement.addUser(jsonData);
+        System.out.println(result);
+
+        // Sample JSON data for adding a user : duplicated username
+        jsonData = "{\"role\": \"client\", \"username\": \"user1\", \"password\": \"1234\", \"email\": \"user1@gmail.com\", \"address\": {\"country\": \"Iran\", \"city\": \"Tehran\"}}";
+        result = UserManagement.addUser(jsonData);
+        System.out.println(result);
+
+        // Sample JSON data for adding a user : duplicated email
+        jsonData = "{\"role\": \"client\", \"username\": \"user2\", \"password\": \"1234\", \"email\": \"user1@gmail.com\", \"address\": {\"country\": \"Iran\", \"city\": \"Tehran\"}}";
+        result = UserManagement.addUser(jsonData);
+        System.out.println(result);
+
+        // Sample JSON data for adding a user : invalid username
+        jsonData = "{\"role\": \"client\", \"username\": \"user@2\", \"password\": \"1234\", \"email\": \"user2@gmail.com\", \"address\": {\"country\": \"Iran\", \"city\": \"Tehran\"}}";
+        result = UserManagement.addUser(jsonData);
+        System.out.println(result);
+
+        // Sample JSON data for adding a user : invalid email
+        jsonData = "{\"role\": \"client\", \"username\": \"user3\", \"password\": \"1234\", \"email\": \"user2gmail.com\", \"address\": {\"country\": \"Iran\", \"city\": \"Tehran\"}}";
+        result = UserManagement.addUser(jsonData);
+        System.out.println(result);
+
+        // Sample JSON data for adding a user : No city
+        jsonData = "{\"role\": \"client\", \"username\": \"user3\", \"password\": \"1234\", \"email\": \"user2@gmail.com\", \"address\": {\"country\": \"Iran\", \"city\": }}";
+        result = UserManagement.addUser(jsonData);
+        System.out.println(result);
     }
+
+
 }
