283981f5e53a591275fc958d416cf94d7d071a7b
Fatemeh Shakiba Baharvand <shakibabaharvand2000@gmail.com>
2024-05-03 12:02:02 +0330

    feat(): cors filter is added

--- CA4/src/main/java/Application/MizDooniApplication.java
+++ CA4/src/main/java/Application/MizDooniApplication.java
@@ -1,3 +1,23 @@
+//package Application;
+//
+//import Service.MizDooni;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.context.annotation.ComponentScan;
+//
+//import java.io.IOException;
+//
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@ComponentScan(basePackages = {"Service", "Controller"})
+//public class MizDooniApplication {
+//
+//    public static void main(String[] args) throws IOException {
+//        MizDooni.getInstance().fetchAndStoreDataFromAPI();
+//        SpringApplication.run(MizDooniApplication.class, args);
+//
+//    }
+//}
 package Application;
 
 import Service.MizDooni;
@@ -5,6 +25,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
 
 import java.io.IOException;
 
@@ -15,6 +39,21 @@ public class MizDooniApplication {
     public static void main(String[] args) throws IOException {
         MizDooni.getInstance().fetchAndStoreDataFromAPI();
         SpringApplication.run(MizDooniApplication.class, args);
+    }
 
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
