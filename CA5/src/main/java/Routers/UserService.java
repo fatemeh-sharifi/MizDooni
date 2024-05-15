67e83f6176edcf0f5ae7036442c5299779094370
Fatemeh Shakiba Baharvand <shakibabaharvand2000@gmail.com>
2024-05-15 12:55:50 +0330

    feat(): /login is added

--- CA5/src/main/java/Routers/UserService.java
+++ CA5/src/main/java/Routers/UserService.java
@@ -3,10 +3,9 @@ package Routers;
 import Entity.User.UserEntity;
 import Repository.User.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +28,28 @@ public class UserService {
             return ResponseEntity.notFound().build();
         } else {
             return ResponseEntity.ok(user);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserEntity> login(
+            @RequestParam String username,
+            @RequestParam String password
+    ) {
+        try {
+            // Query the database to find the user based on the username and password
+            UserEntity user = userRepository.findByUsernameAndPassword(username, password);
+
+            if (user != null) {
+                // Return the user if found
+                return ResponseEntity.ok().body(user);
+            } else {
+                // Return 404 Not Found if user is not found
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            // Return 500 Internal Server Error if an error occurs
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
